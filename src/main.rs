9cfcbf32810dffdd820699762c07c87685c2de31
flyem <62694355+krisMONSTER@users.noreply.github.com>
2022-09-29 20:22:17 +0200

    initial project

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#[macro_use] extern crate rocket;
+
+use rocket::{get, launch};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello world"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
